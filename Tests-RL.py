17cc03a832ac05f6923581075d0bd7bc9b61e794
gastruc <guillaume.astruc.06@gmail.com>
2021-04-20 15:05:14 +0200

    mac

--- Tests-RL.py
+++ Tests-RL.py
@@ -43,7 +43,11 @@ def train_epoch(device):
         t_data = time.time() - iter_data_time
         iter_start_time = time.time()
         print(type(data))
-        print(data.keys)
+        print(data['x'])
+        print(data['y'])
+        print(data['pos'])
+        print(data['gridsize'])
+        print(data['x'][0])
         optimizer.zero_grad()
         data.to(device)
         model.forward(data)
