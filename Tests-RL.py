1ac99f10bbb89e3f489e5110930af687eed3af5c
gastruc <guillaume.astruc.06@gmail.com>
2021-04-21 10:39:08 +0200

    mac

--- Tests-RL.py
+++ Tests-RL.py
@@ -80,7 +80,8 @@ def train_epoch(device):
     for i, data in enumerate(train_loader):
         t_data = time.time() - iter_data_time
         iter_start_time = time.time()
-       
+        print(data['x'].shape)
+        print(type(data['x']))
         data2=batch_to_batch(data)
         
         optimizer.zero_grad()
