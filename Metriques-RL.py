2e59b79667c54304f7fb141f33dcba9cfb43eee4
gastruc <guillaume.astruc.06@gmail.com>
2021-05-03 10:42:49 +0200

    mac

--- Metriques-RL.py
+++ Metriques-RL.py
@@ -322,6 +322,7 @@ for u in [128]:
     tracker = dataset.get_tracker(False, True)
     print("Modèle 128 + 128 aléatoires:")
     test_epoch1_128('cuda',256,0,0)
+    print(tracker.publish(0)['current_metrics'].keys)
     print(tracker.publish(0)['current_metrics']['acc'])
     print(tracker.publish(0)['current_metrics']['loss'])
     
