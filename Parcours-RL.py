fd4b317a7de78cb806972c46ac18abfab83105ec
gastruc <guillaume.astruc.06@gmail.com>
2021-06-28 09:21:46 +0200

    mac

--- Parcours-RL.py
+++ Parcours-RL.py
@@ -118,6 +118,7 @@ class DQN(nn.Module):
         inp = inp.to(device)
         tr=model_128.extract(inp)
         inp.x = inp.x.transpose(1, 2)
+        print(tr['x'][[indice]].shape)
         tr=torch.unsqueeze(tr['x'][[indice]],1)
         tr=torch.squeeze(tr,3)
         tr=self.conv1(tr)
