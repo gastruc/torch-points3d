3ac12c88827676fbf04a89d34cf5941e35e91562
gastruc <guillaume.astruc.06@gmail.com>
2021-07-06 09:52:34 +0200

    mac

--- Parcours-RL.py
+++ Parcours-RL.py
@@ -253,6 +253,7 @@ def parcours(data,state,points,j):
             with torch.no_grad():
                 samp=torch.tensor([[random.random(),random.random(),random.random()]], device=device)
                 result=policy_net(state,j,samp)
+                print(result)
                 print(torch.max(result),torch.argmax(result))
                 l.append((max(result),torch.argmax(result),i,samp))
         try:
